47c65d1706ff71f093c25732b0be545eb7d945bb
Ivounda16 <kombilahogrel@gmail.com>
2020-04-20 10:53:10 +0000

    Updates

---
+++ precache-manifest.cffef490419d4dd9dff87d2e8887a87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47234c57f3c6e76c70082c40d3a646ad",
+    "url": "//github.com/Ivounda16/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fb21d6841c49e590ad62",
+    "url": "//github.com/Ivounda16/monsters-rolodex/static/css/main.4084e9ff.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "//github.com/Ivounda16/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//github.com/Ivounda16/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb21d6841c49e590ad62",
+    "url": "//github.com/Ivounda16/monsters-rolodex/static/js/main.ff466518.chunk.js"
+  },
+  {
+    "revision": "764c8279437fa0e7d869",
+    "url": "//github.com/Ivounda16/monsters-rolodex/static/js/runtime-main.ac41e06c.js"
+  }
+]);
